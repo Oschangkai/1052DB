04d047ab07a524e7022f4905f60207258a193675
Oschangkai <oschangkai@gmail.com>
2017-05-05 12:36:39 +0800

    Test Build

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Description
 A RESTful APIs using Node JS, Express JS, and MS SQL Server
+> Testing Version, lots of bugs
 
 ## Enviroment
 - Node.js v6.10.x (or newer)
--- index.js
+++ index.js
@@ -1,31 +1,74 @@
-const express = require('express');
-const app = express();
-const router = express.Router();  
-const bodyParser = require('body-parser'); 
-const sqlQuery = require('./sqlQuery');
-
 try { // import the sensitive data from config.json
-  global.__conf = require('./config.json');
-} catch(e) { console.log("config file NOT FOUND") };
+  global._conf = require('./config.json');
+} catch(e) { console.log("config file NOT FOUND"); };
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/api', router); // all of routes will be prefixed with /api
+const epilogue = require('epilogue');
+const http = require('http');
 
-router.get('/', function(req, res) {
-    res.json({ message: 'Hooray! API Works!' });
-    console.log("/api GET.");
+// Initialize server
+var server, app;
+if (process.env.USE_RESTIFY) {
+  var restify = require('restify');
+
+  app = server = restify.createServer();
+  app.use(restify.queryParser());
+  app.use(restify.bodyParser());
+} else {
+  var express = require('express'),
+      bodyParser = require('body-parser');
+
+  var app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  server = http.createServer(app);
+};
+
+// Define models
+const Sequelize = require('sequelize');
+const db = new Sequelize(_conf.sql_db, _conf.sql_user, _conf.sql_pwd, {
+  host: _conf.sql_server,
+  dialect: 'mssql'
+});
+const testingDB = db.define("Kai", {
+  col1: Sequelize.INTEGER,
+  col2: Sequelize.CHAR(4)
+},{
+  freezeTableName: true,
+  timestamps: false,
+})
+const Product = db.define("PRODUCT", {
+  P_CODE: Sequelize.CHAR(10),
+  P_DESCRIPT: Sequelize.CHAR(35),
+  P_INDATE: Sequelize.DATE,
+  P_QOH: Sequelize.INTEGER,
+  P_MIN: Sequelize.INTEGER,
+  P_PRICE: Sequelize.DECIMAL(8,2),
+  P_DISCOUNT: Sequelize.DECIMAL(5,2),
+  V_CODE: Sequelize.INTEGER
 });
 
-router.route('/coursesList')
-  .post(function(req, res) {
+// Initialize epilogue
+epilogue.initialize({
+  app: app,
+  sequelize: db
+});
 
-  })
-  .get(function(req, res){
-      sqlQuery.getData(res, "");
+// Create REST resource
+var testing = epilogue.resource({
+  model: testingDB,
+  endpoints: ['/testing', '/testing/:id'],
+  search: 
+    {
+      param: 'testingId',
+      attributes: ['id']
+    }
+});
+
+// Create database and listen
+db
+  .sync({ force: false })
+  .then(function() {
+    server.listen(8008, function() {
+    console.log('listening');
+    });
   });
-
-//"INSERT INTO KaiYi.dbo.testing (col1) VALUES ('2222')"
-var server = app.listen(8008,function(){
- console.log('Server is running!');
-});
